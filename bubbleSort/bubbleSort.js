4f42b96fd027e13e89ba40d12fff86fa72963039
Caiasso <guilherme.caiasso@ufcspa.edu.br>
2024-11-04 16:03:02 -0300

    Correcting a few mistakes on the code organization

--- bubbleSort/bubbleSort.js
+++ bubbleSort/bubbleSort.js
@@ -1,6 +1,9 @@
 "use strict";
 
-const arr = [1, 5, 2, 7, 3, 8, 9, 4, 6, 10];
+const arr = [
+  15, 16, 3, 18, 9, 0, 10, 23, 25, 2, 29, 13, 17, 27, 7, 19, 1, 24, 20, 26, 4,
+  8, 14, 6, 22, 28, 12, 11, 5, 21,
+];
 
 const bubbleSort = (array) => {
   let trocou = true;
@@ -26,5 +29,3 @@ const bubbleSort = (array) => {
 };
 
 console.log(bubbleSort(arr));
-
-export { bubbleSort };
