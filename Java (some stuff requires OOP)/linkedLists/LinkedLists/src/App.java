90d7625ba497d39e0f7298bd4f4d4681014dd658
Caiasso <guilherme.caiasso@ufcspa.edu.br>
2024-11-06 11:07:37 -0300

    Created a brand new folder for some Java coding, things like Linked Lists requires OOP. Yes, it could be done with JS but i like to complicate

---
+++ Java (some stuff requires OOP)/linkedLists/LinkedLists/src/App.java
@@ -0,0 +1,58 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Node nodeA = new Node(0);
+        Node nodeB = new Node(1);
+        Node nodeC = new Node(2);
+        Node nodeD = new Node(3);
+        Node nodeE = new Node(4);
+        Node nodeF = new Node(5);
+
+        nodeA.next = nodeB;
+        nodeB.next = nodeC;
+        nodeC.next = nodeD;
+        nodeD.next = nodeE;
+        nodeE.next = nodeF;
+
+        System.out.println("Linked List size before addition:\n" + countNodes(nodeA) + "\n");
+
+        Node nodeG = new Node(6);
+
+        System.out.println("Adding one extra node:");
+        Node current = nodeA;
+        for (int i = 0; i < countNodes(nodeA); i++) {
+            System.out.print(current.data + (current.next != null ? ", " : " "));
+            current = current.next;
+        }
+
+        System.out.println("\n\nLinked List size after addtion:");
+
+        addNode(nodeA, nodeG);
+
+        System.out.println(countNodes(nodeA));
+
+    }
+
+    public static int countNodes(Node head) {
+        int nodesCount = 0;
+        Node current = head;
+
+        while (current != null) {
+            nodesCount++;
+            current = current.next;
+        }
+
+        return nodesCount;
+    }
+
+    public static void addNode(Node head, Node newNode) {
+        Node current = head;
+        int n = countNodes(head);
+
+        for (int i = 0; i < n - 1; i++) {
+            current = current.next;
+        }
+
+        current.next = newNode;
+    }
+}
